1c68cc2ed6a6ab4898a975c373d2d82b969f3670
eugenetruly <yaugen@gmail.com>
2013-11-19 02:15:11 -0500

    Added bundle configs, modernizer, _Layout, favicon, site.less

---
+++ VP3_MVC5.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace VP3_MVC5.Web.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            // Add @Styles.Render("~/Content/bootstrap") in the <head/> of your _Layout.cshtml view
+            // For Bootstrap theme add @Styles.Render("~/Content/bootstrap-theme") in the <head/> of your _Layout.cshtml view
+            // Add @Scripts.Render("~/bundles/bootstrap") after jQuery in your _Layout.cshtml view
+            // When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js"));
+            
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/bootstrap-theme").Include("~/Content/bootstrap-theme.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.less"));
+        }
+    }
+}
